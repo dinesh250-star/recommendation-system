66668a77a9e57ef1e159451cfdac00ea7c851fc9
AvdhootJadhav <avdhootjadhav002@gmail.com>
2023-03-04 13:06:14 +0530

    feedback-form added

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '../css/feedback.css'
 
 function FeedbackForm() {
 
-    const handleSubmit = () => {
-        console.log("form submitted");
+    const [email, setEmail] = useState("");
+    const [feedback, setFeedback] = useState("");
+
+    function handleFeedback(e){
+        e.preventDefault()
+        console.log(email+" "+feedback);
     }
 
     return (
@@ -15,29 +20,14 @@ function FeedbackForm() {
                 <div className='card-body'>
                     <form>
                         <div className='mb-4'>
-                            <input type="text" className='form-control' placeholder='Amount of Nitrogen in Soil' />
+                            <input type="email" className='form-control' placeholder='Enter your email' onChange={e => setEmail(e.target.value)}/>
                         </div>
-                        <div className='mb-4'>
-                            <input type="text" className='form-control' placeholder='Amount of Phosphorus in Soil' />
+                        <div className='form-floating mb-4'>
+                            <textarea className='form-control' onChange={e => setFeedback(e.target.value)} placeholder="Leave a comment here" id="floatingTextarea"></textarea>
+                            <label htmlFor='floatingTextarea' className='feedback-area'>Feedback</label>
                         </div>
-                        <div className='mb-4'>
-                            <input type="text" className='form-control' placeholder='Amount of Potassium in Soil' />
-                        </div>
-                        <div className='mb-4'>
-                            <input type="text" className='form-control' placeholder='Temperature (in Celcius)' />
-                        </div>
-                        <div className='mb-4'>
-                            <input type="text" className='form-control' placeholder='Humidity (in %)' />
-                        </div>
-                        <div className='mb-4'>
-                            <input type="text" className='form-control' placeholder='pH value of Soil' />
-                        </div>
-                        <div className='mb-4'>
-                            <input type="text" className='form-control' placeholder='Rainfall (in mm)' />
-                        </div>
-                        <button type="submit" class="btn custom-btn" onClick={handleSubmit}>Recommend</button>
+                        <button className='btn custom-btn' onClick={e => handleFeedback(e)}>Submit</button>
                     </form>
-
                 </div>
             </div>
         </div>
